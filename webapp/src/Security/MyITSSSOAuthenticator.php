108fdc3caba9854d39426ff2351a39c06d48b3eb
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-11-07 18:58:56 +0700

    reimplement myits sso auth

--- webapp/src/Security/MyITSSSOAuthenticator.php
+++ webapp/src/Security/MyITSSSOAuthenticator.php
@@ -4,37 +4,45 @@ namespace App\Security;
 use App\Entity\Role;
 use App\Entity\Team;
 use App\Entity\TeamAffiliation;
-use App\Entity\TeamCategory;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
-use Exception;
 use Its\Sso\OpenIDConnectClient;
+use Its\Sso\OpenIDConnectClientException;
 use Symfony\Component\DependencyInjection\ParameterBag\ContainerBagInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 use Symfony\Component\Security\Core\Security;
-use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
-class MyITSSSOAuthenticator extends AbstractGuardAuthenticator
+class MyITSSSOAuthenticator extends AbstractAuthenticator
 {
-    private $params;
-    private $em;
-    private $security;
+    use TargetPathTrait;
+    
+    private ContainerBagInterface $params;
+    private EntityManagerInterface $em;
+    private RouterInterface $router;
+    private Security $security;
 
     public function __construct(
         ContainerBagInterface $params,
         EntityManagerInterface $em,
-        Security $security
+        Security $security,
+        RouterInterface $router
     )
     {
         $this->params = $params;
         $this->em = $em;
         $this->security = $security;
+        $this->router = $router;
     }
 
     /**
@@ -46,122 +54,102 @@ class MyITSSSOAuthenticator extends AbstractGuardAuthenticator
     {
         // if there is already an authenticated user (likely due to the session)
         // then return null and skip authentication: there is no need.
-        return !$this->security->getUser() && $request->attributes->get('_route') === 'oidc';
+        return !$this->security->getUser()
+                && $request->attributes->get('_route') === 'oidc'
+                && $request->isMethod('GET');
     }
 
-    /**
-     * Called on every request. Return whatever credentials you want to
-     * be passed to getUser() as $credentials.
-     */
-    public function getCredentials(Request $request)
+    public function authenticate(Request $request): Passport
     {
-        return true;
-    }
+        $oidc = new OpenIDConnectClient(
+            $this->params->get('openid.provider'), // authorization_endpoint
+            $this->params->get('openid.client_id'), // Client ID
+            $this->params->get('openid.client_secret') // Client Secret
+        );
 
-    public function getUser($credentials, UserProviderInterface $userProvider): ?UserInterface
-    {
-        try {
-            $oidc = new OpenIDConnectClient(
-                $this->params->get('openid.provider'), // authorization_endpoint
-                $this->params->get('openid.client_id'), // Client ID
-                $this->params->get('openid.client_secret') // Client Secret
-            );
-    
-            $oidc->setRedirectURL($this->params->get('openid.redirect_uri')); // must be the same as you registered
-            $oidc->addScope($this->params->get('openid.scope')); //must be the same as you registered
-    
-            if($this->params->get('kernel.environment') === 'dev') {
-                // remove this if in production mode
-                $oidc->setVerifyHost(false);
-                $oidc->setVerifyPeer(false);
-            }
-            $oidc->authenticate(); //call the main function of myITS SSO login
-    
-            $_SESSION['id_token'] = $oidc->getIdToken(); // must be save for check session dan logout proccess
-            $userInfo = $oidc->requestUserInfo(); // this will return user information from myITS SSO database
-    
-            // return new JsonResponse($userInfo);
-            $em = $this->em;
-    
-            $nrp = $userInfo->reg_id;
-            $user = $em->getRepository(User::class)->findOneBy(['username' => $nrp]);
-    
-            if (!$user) {
-                $user = new User();
-                $teamRole = $this->em->getRepository(Role::class)->findOneBy(['dj_role' => 'team']);
-                $user->setUsername($nrp)->addUserRole($teamRole);
-            }
-    
-            $team = $user->getTeam();
-            if (!$team) {
-                $team = new Team();
-                $participantCategory = $this->em->getRepository(TeamCategory::class)->findOneBy(['categoryid' => 3]);
-                $itsAffiliation = $this->em->getRepository(TeamAffiliation::class)->findOneBy(['externalid' => 'its']);
-                $team
-                    ->setIcpcid($nrp)
-                    ->setCategory($participantCategory)
-                    ->setAffiliation($itsAffiliation);
-            }
-    
-            $team
-                ->setName($userInfo->name)
-                ->setDisplayName($userInfo->name)
-                ->setMembers(sprintf("Nama: %s\nNRP: %s", $userInfo->name, $nrp));
-            
-            $em->persist($team);
-            $em->flush();
-    
+        $oidc->setRedirectURL($this->params->get('openid.redirect_uri')); // must be the same as you registered
+        $oidc->addScope($this->params->get('openid.scope')); //must be the same as you registered
+
+        if($this->params->get('kernel.environment') === 'dev') {
+            // remove this if in production mode
+            $oidc->setVerifyHost(false);
+            $oidc->setVerifyPeer(false);
+        }
+        $oidc->authenticate(); //call the main function of myITS SSO login
+
+        $_SESSION['id_token'] = $oidc->getIdToken(); // must be save for check session dan logout proccess
+        $userSso = $oidc->requestUserInfo(); // this will return user information from myITS SSO database
+
+        $em = $this->em;
+
+        $nrp = $userSso->reg_id;
+        /** @var ?User $user */
+        $user = $em->getRepository(User::class)->findOneBy(['externalid' => $nrp]);
+
+        if (!$user) {
+            $user = new User();
+            $teamRole = $this->em->getRepository(Role::class)->findOneBy(['dj_role' => 'team']);
             $user
-                ->setName($userInfo->name)
-                ->setEmail($userInfo->email)
+                ->setUsername($nrp)
+                ->setExternalid($nrp)
                 ->setPlainPassword(random_bytes(16))
                 ->setEnabled(true)
-                ->setTeam($team);
-            
-            $em->persist($user);
-            $em->flush();
-    
-            return $user;
-        } catch(Exception $e) {
-            throw new AuthenticationException($e->getMessage(), $e->getCode(), $e);
+                ->addUserRole($teamRole);
         }
+        
+        /** @var ?Team $team */
+        $team = $em->getRepository(Team::class)->findOneBy(['externalid' => $nrp]);
+        if (!$team) {
+            $team = new Team();
+            $itsAffiliation = $this->em->getRepository(TeamAffiliation::class)->findOneBy(['externalid' => 'its']);
+            $team
+                ->setExternalid($nrp)
+                ->setAffiliation($itsAffiliation);
+        }
+        
+        $team
+            ->setName($userSso->name)
+            ->setDisplayName($userSso->name)
+            ->setPublicDescription(sprintf("Nama: %s\nNRP: %s", $userSso->name, $nrp));
+        $em->persist($team);
+        
+        $user
+            ->setName($userSso->name)
+            ->setTeam($team);
+        $em->persist($user);
+
+        $em->flush();
+
+        return new SelfValidatingPassport(new UserBadge($user->getUsername()));
     }
 
-    public function checkCredentials($credentials, UserInterface $user): bool
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-        // Check credentials - e.g. make sure the password is valid.
-        // In case of an API token, no credential check is needed.
+        // On success, redirect to the last page or the homepage if it was a user triggered action.
+        if ($request->attributes->get('_route') === 'oidc'
+            && $request->isMethod('GET')) {
+            // Use target URL from session if set.
+            if ($firewallName !== null &&
+                $targetUrl = $this->getTargetPath($request->getSession(), $firewallName)) {
+                $this->removeTargetPath($request->getSession(), $firewallName);
+                return new RedirectResponse($targetUrl);
+            }
 
-        // Return `true` to cause authentication success
-        return true;
-    }
-
-    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey): ?Response
-    {
-        // on success, let the request continue
+            return new RedirectResponse($this->router->generate('root'));
+        }
         return null;
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
+        // We only throw an error if the credentials provided were wrong or the user doesn't exist.
+        // Otherwise, we pass along to the next authenticator.
+        if ($exception instanceof OpenIDConnectClientException || $exception instanceof UserNotFoundException) {
+            $resp = new Response('', Response::HTTP_UNAUTHORIZED);
+            return $resp;
+        }
+
+        // Let another guard authenticator handle it.
         return null;
-    }
-
-    /**
-     * Called when authentication is needed, but it's not sent
-     */
-    public function start(Request $request, AuthenticationException $authException = null): Response
-    {
-        $data = [
-            // you might translate this message
-            'message' => 'Authentication Required'
-        ];
-
-        return new JsonResponse($data, Response::HTTP_UNAUTHORIZED);
-    }
-
-    public function supportsRememberMe(): bool
-    {
-        return false;
     }
 }
